4c51aae69d6c98653b1a7f039dfe3342f155e234
Joaocastro20 <joaopedroabreucastro@hotmail.com>
2022-04-12 10:43:09 -0300

    folha

---
+++ folhapagamento/src/test/java/com/folhapagamento/FolhapagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folhapagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhapagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
