f8369af74ca61e3f71ba8f670b8db8e8a507805b
Joaocastro20 <joaopedroabreucastro@hotmail.com>
2022-04-19 10:11:55 -0300

    configuraçao do profile

--- microservice/src/main/java/com/microservice/microservice/resources/WorkerResource.java
+++ microservice/src/main/java/com/microservice/microservice/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import com.microservice.microservice.repository.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,11 +22,16 @@ public class WorkerResource {
 
     private Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment environment;
 
     @Autowired
     private WorkerRepository workerRepository;
+
+
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
@@ -33,7 +39,7 @@ public class WorkerResource {
         return  ResponseEntity.ok(list);
     }
     @GetMapping(value = "/{id}")
-    public  ResponseEntity<Worker> findById(@PathVariable Long id){
+    public  ResponseEntity<Worker> findById(@PathVariable Long id) {
 
         /*int x = 1;
         if(x==1){
@@ -47,7 +53,11 @@ public class WorkerResource {
         logger.info("PORT = " + environment.getProperty("local.server.port"));
 
         Worker obj = workerRepository.findById(id).get();
-        return  ResponseEntity.ok(obj);
+        return ResponseEntity.ok(obj);
     }
-
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs () {
+        logger.info("config = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
 }
